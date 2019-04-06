c87ed2b18fac7deb180ec09309b0534cd05e4183
febydahlan_6706171083 <33033464+febydahlan@users.noreply.github.com>
2019-04-06 11:35:07 +0700

    Update JavaApplication1.java

--- JavaApplication1/src/javaapplication1/JavaApplication1.java
+++ JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -25,12 +25,10 @@ public class JavaApplication1 {
         long fib[] = new long[n];
         fib[0] = 1;
         fib[1] = 1;
-
-         
+        
         for(int i = 2; i < n; i++) {
             fib[i] = fib[i-1] + fib[i-2];
         }
-         
         for (int i =0  ;i < n; i++) {
             System.out.print(fib[i] +  " \n");
             hasil = hasil + (int) fib [i];
